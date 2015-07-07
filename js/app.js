af7295520319d290d970edac3b40b4905462700b
Gmotes <burak.metu03@gmail.com>
2015-07-07 14:40:19 +0300

    Update

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
-var angularWork = angular.module('angularWork',["ngStorage"]);
+var angularWork = angular.module('angularWork',[]);
 
 
-angularWork.controller('angularWorkCtrl',function($scope,$http,$localStorage) {
+angularWork.controller('angularWorkCtrl',function($scope,$http) {
 
-    $scope.TestMenu = MenuArray;
-	
 	$scope.customer = {
     name: 'Naomi',
     address: '1600 Amphitheatre'
@@ -13,10 +11,14 @@ angularWork.controller('angularWorkCtrl',function($scope,$http,$localStorage) {
 
     $scope.caption = "Spor Haberleri";
 	 
-	$http.get('http://localhost:8080/RESTfulExample/rest/json/metallica/get').
+	$http.get('http://localhost:8080/angularTest/menu.json').
         success(function (data) {
-		$scope.jsonSinger = data.singer;
-	});
+		$scope.TestMenu = data;
+	}).error(function(data, status, headers, config) {
+	    console.log(status+":"+data);
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+    });
 	
      $scope.menuClicked = function(menu){
 	   this.menu = menu;
@@ -33,41 +35,6 @@ angularWork.controller('angularWorkCtrl',function($scope,$http,$localStorage) {
 
 
 
-var MenuArray = [ {
-    No : 1,
-	Menu : [ {
-	Caption: "Beşiktaş Haberleri",
-	Path :"index.html",
-	Paragraf : "Beşiktaş yönetimi üçüncü transfer bombasını geceyarısı patlattı ve Dinamo Moskova'nın Brezilyalı stoperi Douglas Teixeira ile anlaştı."
-	
-	} ]
-   }
-   , {
-	No: 2,
-	Menu : [ {
-	Caption: "Fenerbahçe Haberleri",
-	Path :"ourwork.html",
-	Paragraf : "Fenerbahçe, dün akşam İstanbul'a getirdiği Luis Nani'nin imza töreniyle ilgili bir açıklama yaptı."
-	} ]
-  }
-  ,
-  {
-	No: 3,
-	Menu : [ {
-	Caption: "Galatasaray Haberleri",
-	Path :"testimonials.html",
-	Paragraf : "Aurelien Chedjou, Nani'nin İstanbul'a inmesi sonrası arkadaşından gelen bir iletiyi takipçileriyle paylaştı."
-	} ]
-  }
-  ,{
-	No: 4,
-	Menu : [ {
-	Caption: "Projects",
-	Path :"projects.html",
-	Paragraf : ""
-	} ]
-  }
-  ];
 	
 
  
